c11c7d01801d17f2c89a453169280c54ec02e7cf
minefarmer <pgoldfarm@yahoo.com>
2020-07-24 21:11:41 -0500

    Goo night

---
+++ LearnPythonNotes/AssociativityOfOper.py
@@ -0,0 +1,9 @@
+''' Associativity of Python Operators
+This is the order in which an expression is evaluated that has multiple operator of the same precedence.
+Almost all operators have left-right associativity
+
+!!!!!!    ASSOCIATIVITY CAN NOT BE APPLIES TO COMPARISON AND aSSIGNMENT OPERATORS   !!!!!!
+
+
+
+'''
